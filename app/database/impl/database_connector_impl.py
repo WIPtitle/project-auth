8ad16138e324d2e9ffc58fc20464145d1bede314
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-09-16 16:34:35 +0200

    fix: don't crash on duplicated tables

--- app/database/impl/database_connector_impl.py
+++ app/database/impl/database_connector_impl.py
@@ -17,7 +17,10 @@ class DatabaseConnectorImpl(DatabaseConnector):
             f"postgresql://{credentials['POSTGRES_USER']}:{credentials['POSTGRES_PASSWORD']}@{database_hostname}:5432/{credentials['POSTGRES_DB']}",
             echo=True)
 
-        SQLModel.metadata.create_all(self.engine)
+        try:
+            SQLModel.metadata.create_all(self.engine)
+        except:
+            pass
 
     def get_session(self):
         if self.session is None or not self.session.is_active:
