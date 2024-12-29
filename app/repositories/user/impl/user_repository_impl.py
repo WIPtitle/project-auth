7fe98f84c86f3f7c097f1874da2c05b2b1780500
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-12-29 15:21:38 +0100

    feat: db session manual closing

--- app/repositories/user/impl/user_repository_impl.py
+++ app/repositories/user/impl/user_repository_impl.py
@@ -16,7 +16,9 @@ class UserRepositoryImpl(UserRepository):
 
     def find_by_id(self, user_id: int) -> User:
         statement = select(User).where(User.id == user_id)
-        user_db = self.database_connector.get_new_session().exec(statement).first()
+        session = self.database_connector.get_new_session()
+        user_db = session.exec(statement).first()
+        session.close()
         if user_db is None:
             raise NotFoundException("User was not found")
 
@@ -25,7 +27,9 @@ class UserRepositoryImpl(UserRepository):
 
     def find_by_email(self, email: str) -> User:
         statement = select(User).where(User.email == email)
-        user_db = self.database_connector.get_new_session().exec(statement).first()
+        session = self.database_connector.get_new_session()
+        user_db = session.exec(statement).first()
+        session.close()
         if user_db is None:
             raise NotFoundException("User was not found")
 
@@ -36,33 +40,50 @@ class UserRepositoryImpl(UserRepository):
         try:
             self.find_by_email(user.email)
         except NotFoundException:
-            self.database_connector.get_new_session().add(user)
-            self.database_connector.get_new_session().commit()
-            self.database_connector.get_new_session().refresh(user)
+            session = self.database_connector.get_new_session()
+            session.add(user)
+            session.commit()
+            session.refresh(user)
+            session.close()
             return user
         raise BadRequestException("User already exists")
 
 
     def update(self, user: User) -> User:
-        user_db = self.find_by_id(user.id)
+        statement = select(User).where(User.id == user.id)
+        session = self.database_connector.get_new_session()
+        user_db = session.exec(statement).first()
+        if user_db is None:
+            raise NotFoundException("User was not found")
+
         user_db.email = user.email
         if user.password is not None and user.password != "":
             user_db.password = user.password
         if user.pin is not None and user.pin != "":
             user_db.pin = user.pin
         user_db.permissions = user.permissions
-        self.database_connector.get_new_session().commit()
-        self.database_connector.get_new_session().refresh(user_db)
+        session.commit()
+        session.refresh(user_db)
+        session.close()
         return user_db
 
 
     def delete_by_id(self, user_id: int) -> User:
-        user_db = self.find_by_id(user_id)
-        self.database_connector.get_new_session().delete(user_db)
-        self.database_connector.get_new_session().commit()
+        statement = select(User).where(User.id == user_id)
+        session = self.database_connector.get_new_session()
+        user_db = session.exec(statement).first()
+        if user_db is None:
+            raise NotFoundException("User was not found")
+
+        session.delete(user_db)
+        session.commit()
+        session.close()
         return user_db
 
 
     def find_all(self) -> Sequence[User]:
         statement = select(User)
-        return self.database_connector.get_new_session().exec(statement).all()
+        session = self.database_connector.get_new_session()
+        result = session.exec(statement).all()
+        session.close()
+        return result
