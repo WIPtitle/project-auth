ca8284398da73cbd03d66470cd9a028d2926afeb
galvagnimatteo <galvagni.matteo@protonmail.com>
2025-01-18 18:28:48 +0100

    feat(wip): camera always records (WIP)

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,4 +21,16 @@ if [ ! -f "$CREDENTIALS_FILE" ]; then
 EOF
 fi
 
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+uvicorn app.main:app --host 0.0.0.0 --port 8000 &
+
+# Loop until server up to force initialization
+while true; do
+    response=$(curl --write-out "%{http_code}" --silent --output /dev/null http://0.0.0.0:8000)
+    if [ "$response" -eq 404 ]; then
+        break
+    else
+        sleep 1
+    fi
+done
+
+wait
