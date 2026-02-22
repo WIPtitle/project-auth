9c81e9c4f650635b03be9e0a176f600e15f375e2
galvagnimatteo <galvagni.matteo@protonmail.com>
2026-02-22 11:37:26 +0100

    fix: better caching

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-cd /app
-
-pip install --upgrade pip
-
-apt-get update && apt-get install -y libpq-dev git
-pip install --no-cache-dir -r requirements.txt
-
 CREDENTIALS_FILE=${AUTH_SECRET_KEY_FILE}
 echo "Searching secret key in ${AUTH_SECRET_KEY_FILE}"
 
@@ -21,16 +14,4 @@ if [ ! -f "$CREDENTIALS_FILE" ]; then
 EOF
 fi
 
-uvicorn app.main:app --host 0.0.0.0 --port 8000 &
-
-# Loop until server up to force initialization
-while true; do
-    response=$(curl --write-out "%{http_code}" --silent --output /dev/null http://0.0.0.0:8000)
-    if [ "$response" -eq 404 ]; then
-        break
-    else
-        sleep 1
-    fi
-done
-
-wait
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000
